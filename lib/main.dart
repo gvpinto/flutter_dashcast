ebb5371ad8754d656adbe1d1fe38fb26b45397c7
Glenn Pinto <gvpinto@gmail.com>
2022-11-05 13:11:55 -0400

    downloading and saving the file

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,34 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:webfeed/webfeed.dart';
 import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as path;
 
 final Uri url = Uri.parse("https://itsallwidgets.com/podcast/feed");
+
+const pathSuffix = '/dashcast/downloads';
+
+Future<String> _getDownloadPath(String filename) async {
+  final dir = await getApplicationDocumentsDirectory();
+  final prefix = dir.path;
+  final filePath = path.join(prefix, filename);
+  print('FilePath: ${filePath}');
+  return filePath;
+}
 
 class PodCast with ChangeNotifier {
   RssFeed? _feed;
   RssItem? _selectedItem;
+  Map<String, bool>? downloadStatus;
 
   void parse(Uri url) async {
     final response = await http.get(url);
-    String? xmlString;
-    xmlString = response.body;
+    String? xmlString = response.body;
     _feed = RssFeed.parse(xmlString);
     notifyListeners();
   }
@@ -32,6 +45,26 @@ class PodCast with ChangeNotifier {
   set selectedItem(RssItem? value) {
     _selectedItem = value;
     notifyListeners();
+  }
+
+  Future<void> download(RssItem item) async {
+    var client = http.Client();
+
+    final res = await client.send(http.Request('GET', Uri.parse(item.guid!)));
+
+    if (res.statusCode != 200) {
+      throw Exception('Unexpected HTTP code: ${res.statusCode}');
+    }
+
+    final file = File(await _getDownloadPath(path.split(item.guid!).last));
+    res.stream.pipe(file.openWrite()).whenComplete(() {
+      client.close();
+      print('Download Complete');
+    });
+
+    // res.stream.listen((value) {
+    //   print('Bytes: ${value.length}');
+    // });
   }
 }
 
@@ -93,6 +126,26 @@ class EpisodeListView extends StatelessWidget {
                 i.description!,
                 maxLines: 3,
                 overflow: TextOverflow.ellipsis,
+              ),
+              trailing: IconButton(
+                icon: const Icon(Icons.arrow_downward),
+                onPressed: () {
+                  try {
+                    context.read<PodCast>().download(i);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Downloading! ${i.title}'),
+                      ),
+                    );
+                  } on Exception catch (e) {
+                    print(e);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Error when downloading'),
+                      ),
+                    );
+                  }
+                },
               ),
               onTap: () {
                 context.read<PodCast>().selectedItem = i;
@@ -241,6 +294,8 @@ class _PlaybackButtonState extends State<PlaybackButton> {
     } else {
       // const url =
       //     'https://cdn.pixabay.com/download/audio/2022/10/12/audio_061cead49a.mp3?filename=weeknds-122592.mp3';
+      const url =
+          '/Users/gvpinto/Library/Developer/CoreSimulator/Devices/ED04ABB2-6F8D-426E-AD55-89DFCECAD7D9/data/Containers/Data/Application/4F87065C-1EFB-4802-AA53-E02F0C229082/Documents/episode-43.mp3';
       Duration? d = await _myPlayer.startPlayer(fromURI: url);
       _myPlayer.setSubscriptionDuration(const Duration(milliseconds: 1000));
 
